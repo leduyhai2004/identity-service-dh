c4c57205f90f09d2f7ec6f66a20b3b7f669c92cd
Le Duy Hai <duyha0207@gmail.com>
2025-07-30 16:26:08 +0700

    add

---
+++ src/main/java/com/dh/identityservice/entity/Address.java
@@ -0,0 +1,24 @@
+package com.dh.identityservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    String street;
+
+}
